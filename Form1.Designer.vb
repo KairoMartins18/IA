b92c04836494b82dbe2297e2224ec367a0dbf075
Kairo Martins <kairomartinscontact@gmail.com>
2020-06-05 10:45:01 -0400

    Adicionado
    Alguns novos recursos

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -29,13 +29,16 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.lbuser = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(245, 188)
+        Me.Label1.Location = New System.Drawing.Point(274, 62)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(311, 31)
         Me.Label1.TabIndex = 0
@@ -43,17 +46,17 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(84, 303)
+        Me.TextBox1.Location = New System.Drawing.Point(21, 30)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(399, 54)
+        Me.TextBox1.Size = New System.Drawing.Size(372, 43)
         Me.TextBox1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(201, 380)
+        Me.Button1.Location = New System.Drawing.Point(416, 30)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(169, 38)
+        Me.Button1.Size = New System.Drawing.Size(169, 43)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Enviar"
         Me.Button1.UseVisualStyleBackColor = True
@@ -61,7 +64,7 @@ Partial Class Form1
         'LinkLabel1
         '
         Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Location = New System.Drawing.Point(425, 393)
+        Me.LinkLabel1.Location = New System.Drawing.Point(515, 78)
         Me.LinkLabel1.Name = "LinkLabel1"
         Me.LinkLabel1.Size = New System.Drawing.Size(70, 13)
         Me.LinkLabel1.TabIndex = 3
@@ -72,7 +75,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(39, 188)
+        Me.Label2.Location = New System.Drawing.Point(136, 62)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(59, 31)
         Me.Label2.TabIndex = 4
@@ -82,36 +85,58 @@ Partial Class Form1
         '
         Me.lbuser.AutoSize = True
         Me.lbuser.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lbuser.Location = New System.Drawing.Point(104, 188)
+        Me.lbuser.Location = New System.Drawing.Point(201, 62)
         Me.lbuser.Name = "lbuser"
-        Me.lbuser.Size = New System.Drawing.Size(144, 31)
+        Me.lbuser.Size = New System.Drawing.Size(30, 31)
         Me.lbuser.TabIndex = 5
-        Me.lbuser.Text = "%USER%"
+        Me.lbuser.Text = "0"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(248, 45)
+        Me.Label4.Location = New System.Drawing.Point(12, 9)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(106, 108)
         Me.Label4.TabIndex = 6
         Me.Label4.Text = ":)"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.LinkLabel1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 330)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(597, 100)
+        Me.Panel1.TabIndex = 7
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"//Digite  Help, Para uma visão Geral  dos Comandos Suportados."})
+        Me.ListBox1.Location = New System.Drawing.Point(12, 142)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(555, 160)
+        Me.ListBox1.TabIndex = 8
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(597, 430)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.lbuser)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.LinkLabel1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -124,4 +149,6 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents lbuser As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -14,43 +14,122 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+
+
+
         If TextBox1.Text = "oi" Then
             falar.Speak("olá")
+            ListBox1.Items.Add(TextBox1.Text)
+            ListBox1.Items.Add("Olá")
             TextBox1.Clear()
+            Exit Sub
+
+        Else
+
         End If
 
         If TextBox1.Text = "como vai?" Then
             falar.Speak("Bem, estou sempre atualizando e Você")
+            ListBox1.Items.Add(TextBox1.Text)
+            ListBox1.Items.Add("Bem, estou sempre atualizando e Você")
             TextBox1.Clear()
+            Exit Sub
+        Else
+
         End If
 
         If TextBox1.Text = "bem" Then
             falar.Speak("Que ôtimo")
+            ListBox1.Items.Add(TextBox1.Text)
+            ListBox1.Items.Add("Que ôtimo!")
             TextBox1.Clear()
+            Exit Sub
+
+        Else
+
         End If
 
         If TextBox1.Text = "você conhece a Cortana?" Then
             falar.Speak("Sim, não Gosto muito dela não eu em.")
+            ListBox1.Items.Add(TextBox1.Text)
+            ListBox1.Items.Add("Sim,Não gosto muito dela, EU EM.")
             TextBox1.Clear()
+            Exit Sub
+
+        Else
+
         End If
 
         If TextBox1.Text = "reset" Then
             My.Settings.Reset()
             TextBox1.Clear()
+            Exit Sub
+
+        Else
+
         End If
 
         If TextBox1.Text = "qual é o meu nome?" Then
             falar.Speak(My.Settings.noeuser)
+            ListBox1.Items.Add(My.Settings.noeuser)
             TextBox1.Clear()
+            Exit Sub
+
+        Else
+
         End If
 
+
         If TextBox1.Text = "qual é o seu nome?" Then
-            falar.Speak("Maria roberta de segunda de Constantino fernanda de Guanabara.")
+            falar.Speak("Maria Cyberquantun.")
+            ListBox1.Items.Add(TextBox1.Text)
+            ListBox1.Items.Add("Maria Cyberquantun")
             TextBox1.Clear()
+            Exit Sub
+
+        Else
         End If
+
+        If TextBox1.Text = "Help" Then
+            MsgBox("       Visão Geral de Comandos
+                    oi
+                    qual é o seu nome?
+                    qual é o meu nome?
+                    Você conhece a Cortana?
+                    Como vai?")
+            Exit Sub
+        End If
+
+        If TextBox1.Text >= "" Then
+            ListBox1.Items.Add("Pesquisa: " + TextBox1.Text)
+            diferente()
+            Exit Sub
+        End If
+
+
+
+
+
     End Sub
+
+    Private Sub diferente()
+        falar.Speak("Achei Alguns Resutados ")
+        Process.Start("https://www.google.com/search?q=" + TextBox1.Text)
+        TextBox1.Clear()
+
+    End Sub
+
+
+
 
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Form2.Show()
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
     End Sub
 End Class
